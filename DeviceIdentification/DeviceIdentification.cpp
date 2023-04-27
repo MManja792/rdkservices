df17dd4e35049eb9b394f26a6a7173c69585511e
MManja792 <MManja792@comcast.com>
2023-04-27 12:07:02 -0400

    XIONE-11381 build error resolved

--- DeviceIdentification/DeviceIdentification.cpp
+++ DeviceIdentification/DeviceIdentification.cpp
@@ -20,14 +20,7 @@
 #include "DeviceIdentification.h"
 #include "IdentityProvider.h"
 #include <interfaces/IConfiguration.h>
-
-#pragma once
-
-#include <syscall.h>
-
-#define LOGINFO(fmt, ...) do { fprintf(stderr, "[%d] INFO [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
-#define LOGWARN(fmt, ...) do { fprintf(stderr, "[%d] WARN [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
-#define LOGERR(fmt, ...) do { fprintf(stderr, "[%d] ERROR [%s:%d] %s: " fmt "\n", (int)syscall(SYS_gettid), WPEFramework::Core::FileNameOnly(__FILE__), __LINE__, __FUNCTION__, ##__VA_ARGS__); fflush(stderr); } while (0)
+#include "UtilsLogging.h"
 
 
 #define API_VERSION_NUMBER_MAJOR 1
